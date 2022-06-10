65ab3927ed0cfceb9f9b614a54c3f7f23fdcc636
不做码农 <599854767@qq.com>
2022-06-10 08:25:42 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,74 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+<script setup>
+import { version as vueVersion } from "vue";
+import { version as EpVersion } from "element-plus";
+import { ElementPlus } from "@element-plus/icons-vue";
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+import { reactive } from "vue";
+// const msg = ref("Hello World!");
+const form = reactive({
+  name: "",
+  region: "",
+  date1: "",
+  date2: "",
+  delivery: false,
+  type: [],
+  resource: "",
+  desc: "",
+});
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+const onSubmit = () => {
+  console.log("submit!");
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <el-config-provider size="small">
+    <el-form :model="form" label-width="120px">
+      <el-form-item label="Activity name">
+        <el-input v-model="form.name" />
+      </el-form-item>
+      <el-form-item label="Activity zone">
+        <el-select v-model="form.region" placeholder="please select your zone">
+          <el-option label="Zone one" value="shanghai" />
+          <el-option label="Zone two" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="Activity time">
+        <el-col :span="11">
+          <el-date-picker
+            v-model="form.date1"
+            type="date"
+            placeholder="Pick a date"
+            style="width: 100%"
+          />
+        </el-col>
+        <el-col :span="2" class="text-center">
+          <span class="text-gray-500">-</span>
+        </el-col>
+        <el-col :span="11">
+          <el-time-picker
+            v-model="form.date2"
+            placeholder="Pick a time"
+            style="width: 100%"
+          />
+        </el-col>
+      </el-form-item>
+      <el-form-item label="Instant delivery">
+        <el-switch v-model="form.delivery" />
+      </el-form-item>
+
+      <el-form-item label="Activity form">
+        <el-input v-model="form.desc" type="textarea" />
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">Create</el-button>
+        <el-button>Cancel</el-button>
+      </el-form-item>
+    </el-form>
+
+    <p>
+      <el-icon color="var(--el-color-primary)"><ElementPlus /></el-icon>
+      Element Plus {{ EpVersion }} + Vue {{ vueVersion }}
+    </p>
+  </el-config-provider>
+</template>
